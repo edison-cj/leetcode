eb58c2e1ab05704a0844e015c6438c193d850474
edison-cj <1356366163@qq.com>
2023-07-13 18:22:01 +0800

    2023-7-13

--- src/main/java/daily/DailyTest.java
+++ src/main/java/daily/DailyTest.java
@@ -378,6 +378,31 @@ class Solution {
         return sum * -flag;
     }
 
+    /*
+     * @description: 931. 下降路径最小和
+     * @author: edison 
+     * @date: 2023/7/13 17:31
+     * @param: [matrix]
+     * @return: int
+     */
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) dp[0][i] = matrix[0][i];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j];
+                else if (j == n - 1) dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + matrix[i][j];
+                else dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i -1][j -1], dp[i - 1][j + 1])) + matrix[i][j];
+            }
+        }
+        int ans = dp[n - 1][0];
+        for (int j = 1; j < n; j++) {
+            ans = Math.min(ans, dp[n - 1][j]);
+        }
+        return ans;
+    }
+
 }
 
 class TreeNode {
