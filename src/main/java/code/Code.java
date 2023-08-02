de5b97da090eb185a3a102df8bcd642272dcf188
edison-cj <1356366163@qq.com>
2023-08-02 17:56:16 +0800

    2023-8-2

--- src/main/java/code/Code.java
+++ src/main/java/code/Code.java
@@ -1,5 +1,8 @@
 package code;
 
+import org.junit.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import java.util.*;
 
 /**
@@ -12,6 +15,31 @@ public class Code {
 
     Solution solution = new Solution();
 
+    @Test
+    public void test() {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        int[] a = new int[58];
+        int[] b = new int[58];
+        for (char ch : t.toCharArray()) {
+            a[ch - 'A']++;
+        }
+        int left = 0, right = 0, count = 0;
+        String str = "";
+        while (right < s.length()) {
+            b[s.charAt(right) - 'A']++;
+            if (b[s.charAt(right) - 'A'] <= a[s.charAt(right) - 'A']) count++;
+            while (left <= right && b[s.charAt(left) - 'A'] > a[s.charAt(left) - 'A']) {
+                b[s.charAt(left) - 'A']--;
+                left++;
+            }
+            if (count == t.length() && (str.isEmpty() || str.length() > right - left + 1)) {
+                str = s.substring(left , right + 1);
+            }
+            right++;
+        }
+        System.out.println(str);
+    }
 
 }
 
@@ -134,7 +162,460 @@ class Solution {
         return dummy.next;
     }
 
-    
+    /*
+     * @description: 15. 三数之和
+     * @author: edison 
+     * @date: 2023/8/2 10:23
+     * @param: [nums]
+     * @return: java.util.List<java.util.List<java.lang.Integer>>
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) left++;
+                else if (sum > 0) right--;
+                else {
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return list;
+    }
+
+    /*
+     * @description: 5. 最长回文子串
+     * @author: edison 
+     * @date: 2023/8/2 10:31
+     * @param: [s]
+     * @return: java.lang.String
+     */
+    public String longestPalindrome(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int index = 0;
+        int len = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i <= 1) dp[i][j] = true;
+                    else if (dp[i + 1][j - 1]) dp[i][j] = true;
+                }
+                if (dp[i][j] && j - i + 1 > len) {
+                    index = i;
+                    len = j - i + 1;
+                }
+            }
+        }
+        return s.substring(index, index + len);
+    }
+
+    /*
+     * @description: 17. 电话号码的字母组合
+     * @author: edison 
+     * @date: 2023/8/2 10:58
+     * @param: [digits]
+     * @return: java.util.List<java.lang.String>
+     */
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        if (digits.length() == 0 || digits == null) return list;
+        String[] str = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        backTrack(digits, str, list, 0);
+        return list;
+    }
+    StringBuilder sb = new StringBuilder();
+    void backTrack(String digits, String[] str, List<String> list, int index) {
+        if (index == digits.length()) {
+            list.add(sb.toString());
+            return;
+        }
+        String string = str[digits.charAt(index) - '0'];
+        for (int i = 0; i < string.length(); i++) {
+            sb.append(string.charAt(i));
+            backTrack(digits, str, list, index + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
+    /*
+     * @description: 19. 删除链表的倒数第 N 个结点
+     * @author: edison 
+     * @date: 2023/8/2 11:09
+     * @param: [head, n]
+     * @return: code.ListNode
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        while (n-- >= 0) fast = fast.next;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+    /*
+     * @description: 20. 有效的括号
+     * @author: edison 
+     * @date: 2023/8/2 11:14
+     * @param: [s]
+     * @return: boolean
+     */
+    public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case ')':
+                    if (deque.isEmpty() || deque.pop() != '(') return false;
+                    break;
+                case ']':
+                    if (deque.isEmpty() || deque.pop() != '[') return false;
+                    break;
+                case '}':
+                    if (deque.isEmpty() || deque.pop() != '{') return false;
+                    break;
+                default: deque.push(ch);
+                    break;
+            }
+        }
+        return deque.isEmpty();
+    }
+
+    /*
+     * @description: 21. 合并两个有序链表
+     * @author: edison 
+     * @date: 2023/8/2 11:33
+     * @param: [list1, list2]
+     * @return: code.ListNode
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    /*
+     * @description: 39. 组合总和
+     * @author: edison 
+     * @date: 2023/8/2 11:46
+     * @param: [candidates, target]
+     * @return: java.util.List<java.util.List<java.lang.Integer>>
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+        backTrack(list, path, candidates, target, 0);
+        return list;
+    }
+    void backTrack(List<List<Integer>> list, LinkedList<Integer> path, int[] candidates, int target, int index) {
+        if (target == 0) {
+            list.add(new ArrayList<>(path));
+            return;
+        }
+        if (target < 0) return;
+        for (int i = index; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            backTrack(list, path, candidates, target - candidates[i], i);
+            path.removeLast();
+        }
+    }
+
+    /*
+     * @description: 42. 接雨水
+     * @author: edison 
+     * @date: 2023/8/2 11:53
+     * @param: [height]
+     * @return: int
+     */
+    public int trap(int[] height) {
+        if (height.length <= 2) return 0;
+        int ans = 0;
+        Deque<Integer> deque = new LinkedList<>();
+        deque.push(0);
+        for (int i = 1; i < height.length; i++) {
+            if (height[i] < height[deque.peek()]) deque.push(i);
+            else if (height[i] == height[deque.peek()]) {
+                deque.pop();
+                deque.push(i);
+            } else {
+                while (!deque.isEmpty() && height[i] > height[deque.peek()]) {
+                    int mid = deque.pop();
+                    if (!deque.isEmpty()) {
+                        int h = Math.min(height[deque.peek()], height[i]) - height[mid];
+                        int w = i - deque.peek() - 1;
+                        ans += h * w;
+                    }
+                }
+                deque.push(i);
+            }
+        }
+        return ans;
+    }
+
+    /*
+     * @description: 46. 全排列
+     * @author: edison
+     * @date: 2023/8/2 15:07
+     * @param: [nums]
+     * @return: java.util.List<java.util.List<java.lang.Integer>>
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+        backTrack(nums, list, path, used);
+        return list;
+    }
+    void backTrack(int[] nums, List<List<Integer>> list, LinkedList<Integer> path, boolean[] used) {
+        if (path.size() == nums.length) {
+            list.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            path.add(nums[i]);
+            used[i] = true;
+            backTrack(nums, list, path, used);
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+
+    /*
+     * @description: 48. 旋转图像
+     * @author: edison
+     * @date: 2023/8/2 15:26
+     * @param: [matrix]
+     * @return: void
+     */
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        int left = 0;
+        int right = matrix.length - 1;
+        while (left < right) {
+            for (int i = 0; i < matrix.length; i++) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+            }
+            left++;
+            right--;
+        }
+    }
+
+    /*
+     * @description: 49. 字母异位词分组
+     * @author: edison
+     * @date: 2023/8/2 15:42
+     * @param: [strs]
+     * @return: java.util.List<java.util.List<java.lang.String>>
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String string = new String(ch);
+            List<String> list = map.getOrDefault(string, new ArrayList<>());
+            list.add(str);
+            map.put(string, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    /*
+     * @description: 53. 最大子数组和
+     * @author: edison 
+     * @date: 2023/8/2 15:53
+     * @param: [nums]
+     * @return: int
+     */
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            if (dp[i] > ans) ans = dp[i];
+        }
+        return ans;
+    }
+
+    /*
+     * @description: 55. 跳跃游戏
+     * @author: edison 
+     * @date: 2023/8/2 15:58
+     * @param: [nums]
+     * @return: boolean
+     */
+    public boolean canJump(int[] nums) {
+        int len = 0;
+        for (int i = 0; i <= len; i++) {
+            int count = i + nums[i];
+            len = Math.max(len, count);
+            if (len >= nums.length - 1) return true;
+        }
+        return false;
+    }
+
+    /*
+     * @description: 56. 合并区间
+     * @author: edison 
+     * @date: 2023/8/2 16:08
+     * @param: [intervals]
+     * @return: int[][]
+     */
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> list = new ArrayList<>();
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > end) {
+                list.add(new int[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            } else {
+                end = Math.max(end, intervals[i][1]);
+            }
+        }
+        list.add(new int[]{start, end});
+        return list.toArray(new int[0][]);
+    }
+
+    /*
+     * @description: 62. 不同路径
+     * @author: edison 
+     * @date: 2023/8/2 16:16
+     * @param: [m, n]
+     * @return: int
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /*
+     * @description: 64. 最小路径和
+     * @author: edison
+     * @date: 2023/8/2 16:21
+     * @param: [grid]
+     * @return: int
+     */
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for (int j = 1; j < n; j++) dp[0][j] = dp[0][j - 1] + grid[0][j];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /*
+     * @description: 70. 爬楼梯
+     * @author: edison 
+     * @date: 2023/8/2 16:27
+     * @param: [n]
+     * @return: int
+     */
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+
+    /*
+     * @description: 75. 颜色分类
+     * @author: edison 
+     * @date: 2023/8/2 16:38
+     * @param: [nums]
+     * @return: void
+     */
+    public void sortColors(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        int t = 0;
+        while (i <= j) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i++] = nums[t];
+                nums[t++] = temp;
+            } else if (nums[i] == 2) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j--] = temp;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    /*
+     * @description: 76. 最小覆盖子串
+     * @author: edison 
+     * @date: 2023/8/2 17:35
+     * @param: [s, t]
+     * @return: java.lang.String
+     */
+    public String minWindow(String s, String t) {
+        int[] a = new int[58];
+        int[] b = new int[58];
+        int left = 0, right = 0, count = 0;
+        String str = "";
+        for (char ch : t.toCharArray()) a[ch - 'A']++;
+        while (right < s.length()) {
+            b[s.charAt(right) - 'A']++;
+            if (b[s.charAt(right) - 'A'] <= a[s.charAt(right) - 'A']) count++;
+            while (left < right && b[s.charAt(left) - 'A'] > a[s.charAt(left) - 'A']) {
+                b[s.charAt(left) - 'A']--;
+                left++;
+            }
+            if (count == t.length() && (str.isEmpty() || str.length() > right - left + 1)) str = s.substring(left, right + 1);
+            right++;
+        }
+        return str;
+    }
 }
 
 class ListNode {
